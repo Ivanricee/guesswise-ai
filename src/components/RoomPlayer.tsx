0341d456251efd5766549fa4abd2e7273c09c463
Iván Bautista <ivanbautistag@gmail.com>
2024-10-15 17:43:28 -0600

    feat: create room for players

---
+++ src/components/RoomPlayer.tsx
@@ -0,0 +1,37 @@
+'use client'
+import { useAppStore } from '@/store/zustand-store'
+import PlayerBoard from './PlayerBoard'
+import ShareToken from './ShareToken'
+import { useSearchParams } from 'next/navigation'
+import { getRoomFromToken } from '@/app/actions/token'
+import { useEffect, useState } from 'react'
+
+export default function RoomPlayer() {
+  const { token } = useAppStore()
+  const searchParams = useSearchParams()
+  const [room, setRoom] = useState<string | null>(null)
+  const inviteToken = searchParams.get('invitation')
+  const validToken = token ?? inviteToken
+  useEffect(() => {
+    const getRoom = async () => {
+      try {
+        if (validToken) {
+          const room = await getRoomFromToken(validToken)
+          setRoom(room)
+        }
+      } catch (error) {
+        console.error('Error al obtener la sala:', error)
+        // Maneja el error según sea necesario
+      }
+    }
+
+    getRoom()
+  }, [validToken])
+
+  return (
+    <section>
+      {room && validToken && <PlayerBoard room={room} />}
+      {token && <ShareToken token={token} />}
+    </section>
+  )
+}
