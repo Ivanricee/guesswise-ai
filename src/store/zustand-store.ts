5da56cda3822a77e699619581c29ff302b284c12
Iván Bautista <ivanbautistag@gmail.com>
2024-10-15 17:48:10 -0600

    feat: add store with zustand, player and token

---
+++ src/store/zustand-store.ts
@@ -0,0 +1,30 @@
+import { create } from 'zustand'
+
+export type Player = {
+  name: string
+  imageUrl: string
+  isHost: boolean
+  isGuessing: boolean
+  isCurrent: boolean
+}
+interface State {
+  players: Player[] | null
+  token: string | null
+}
+interface Actions {
+  setPlayer: ({ player, token }: { player: Player; token: string | null }) => void
+}
+
+export const useAppStore = create<State & Actions>((set) => ({
+  players: null,
+  token: null,
+  setPlayer: ({ player, token }: { player: Player; token: string | null }) =>
+    set((prevState) => {
+      if (prevState.players) {
+        const prevPlayers = structuredClone(prevState.players)
+        if (token) return { players: [...prevPlayers, { ...player }], token }
+        return { players: [...prevPlayers, { ...player }] }
+      }
+      return { players: [{ ...player }], token }
+    }),
+}))
