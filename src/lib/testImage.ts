da861bd5c091d3bb68d59bb380014b1c37b571bb
Iván Bautista <ivanbautistag@gmail.com>
2024-10-15 17:47:18 -0600

    feat: local db for preselected images with tags

---
+++ src/lib/testImage.ts
@@ -0,0 +1,21 @@
+import { CloudinaryImage } from './cloudinary'
+
+export const testImages: CloudinaryImage[] = [
+  {
+    asset_id: 'e35dac5a4dfd8a34329fde3fd055db79',
+    url: 'http://res.cloudinary.com/ivanrice-c/image/upload/v1728798386/Frontend/guesswise/players/local/atest_ai_image_ae74ck.png',
+    height: 521,
+    width: 782,
+    tags: [
+      'left-face',
+      'left-hand',
+      'left-lower-arm',
+      'left-upper-arm',
+      'right-face',
+      'right-hand',
+      'right-lower-arm',
+      'right-upper-arm',
+      'torso',
+    ],
+  },
+]
