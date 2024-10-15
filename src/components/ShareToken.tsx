aa755b87a90578773057dbc0d20aded294c365d0
Iván Bautista <ivanbautistag@gmail.com>
2024-10-15 17:43:50 -0600

    feat: create component for sharing token

---
+++ src/components/ShareToken.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import { copyToClipboard } from '@/lib/utils'
+import { Input } from './ui/input'
+import { Button } from './ui/button'
+
+export default function ShareToken({ token }: { token: string | null }) {
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || ''
+  const invitationUrl = `${baseUrl}?invitation=${token}`
+
+  const handleClick = async () => {
+    await copyToClipboard(invitationUrl)
+  }
+  console.log('CopyToken component:', { token })
+
+  return (
+    <section className="flex bg-stone-950/20 ">
+      {token && (
+        <>
+          <Button onClick={handleClick}>Copiar invitación</Button>
+          <Input value={invitationUrl} disabled />
+        </>
+      )}
+    </section>
+  )
+}
