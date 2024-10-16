9edbe85b06c9a1a170bccd6c00260591dc2b398a
Iván Bautista <ivanbautistag@gmail.com>
2024-10-16 16:08:42 -0600

    subscribe to the room with a new player and

--- src/components/hooks/usePresence.ts
+++ src/components/hooks/usePresence.ts
@@ -1,27 +1,30 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-import { supabaseClient } from '@/lib/supabase'
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { supabaseClient, UserPresence } from '@/lib/supabase'
 import { useAppStore } from '@/store/zustand-store'
 import { RealtimeChannel } from '@supabase/supabase-js'
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
 
 interface UsePresence {
   room: string
 }
 export const usePresence = ({ room }: UsePresence) => {
   const { setPlayer, players } = useAppStore()
-
+  const roomPlayersRef = useRef<RealtimeChannel | null>(null)
   useEffect(() => {
-    let roomPlayers: RealtimeChannel
-    if (players && players?.length > 0) {
-      roomPlayers = supabaseClient.channel(room)
+    if (!roomPlayersRef.current && players && players.length > 0) {
+      const roomPlayers = supabaseClient.channel(room)
+      roomPlayersRef.current = roomPlayers
       roomPlayers
         .on('presence', { event: 'sync' }, () => {
-          const newState = roomPlayers.presenceState()
-          console.log('sync', newState)
+          const state = roomPlayers.presenceState()
+
+          console.log('sync ', { state })
         })
         .on('presence', { event: 'join' }, ({ newPresences }) => {
-          //key
-          const { image, /*tags,*/ user } = newPresences[0]
+          console.log('JOIN', { newPresences })
+
+          const { image, tags, user } = newPresences[0]
+
           const foundPlayer = players.some(
             (player) => `${player.imageUrl}${player.name}` === `${image}${user}`
           )
@@ -30,6 +33,7 @@ export const usePresence = ({ room }: UsePresence) => {
             const player = {
               name: user,
               imageUrl: image,
+              tags,
               isHost: false,
               isGuessing: false,
               isCurrent: false,
@@ -40,13 +44,27 @@ export const usePresence = ({ room }: UsePresence) => {
         .on('presence', { event: 'leave' }, ({ key, leftPresences }) => {
           console.log('leave', key, leftPresences)
         })
-        .subscribe()
+        .subscribe(async (status) => {
+          if (status === 'SUBSCRIBED') {
+            const player = players[0]
+            const newPlayer: UserPresence = {
+              user: player.name,
+              image: player.imageUrl,
+              tags: player.tags,
+            }
+            const presenceTrackStatus = await roomPlayers.track(newPlayer)
+            console.log('tracked:', presenceTrackStatus)
+          } else if (status === 'TIMED_OUT') {
+            console.error('Subscription timed out:', room)
+          } else if (status === 'CLOSED') {
+            console.warn('Subscription closed:', room)
+          } else if (status === 'CHANNEL_ERROR') {
+            console.error('Channel error while subscribing to room:', room)
+          }
+        })
     }
-    return () => {
-      if (players && players?.length > 0) {
-        roomPlayers.unsubscribe()
-      }
-    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [players])
+
   return { players }
 }
