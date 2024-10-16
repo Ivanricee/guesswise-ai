3e264ca5a743249a6bfb21848aac3162bab2e460
Iván Bautista <ivanbautistag@gmail.com>
2024-10-16 16:05:01 -0600

    feat: custom hook to  validate max players

---
+++ src/components/hooks/usePlayerLimit.ts
@@ -0,0 +1,34 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { getRoomFromToken } from '@/app/actions/token'
+import { supabaseClient } from '@/lib/supabase'
+import { useEffect, useState } from 'react'
+
+export const usePlayerLimit = ({ token }: { token: string | null }) => {
+  const [isInlimit, setIsInlimit] = useState<null | boolean>(null)
+  const maxPlayers = 5
+  useEffect(() => {
+    const checkPlayers = async () => {
+      if (token) {
+        const room = (await getRoomFromToken(token)) || ''
+        const roomPlayers = supabaseClient.channel(room)
+        roomPlayers
+          .on('presence', { event: 'sync' }, () => {
+            const state = roomPlayers.presenceState()
+            const countPlayers = Object.keys(state).length
+            if (countPlayers >= maxPlayers) {
+              setIsInlimit(false)
+            } else {
+              setIsInlimit(true)
+            }
+            return roomPlayers.unsubscribe()
+          })
+          .subscribe()
+      } else {
+        return setIsInlimit(true)
+      }
+    }
+
+    checkPlayers()
+  }, [])
+  return { isInlimit }
+}
