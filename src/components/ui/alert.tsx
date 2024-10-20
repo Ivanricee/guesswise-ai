686d90b301003e6a0cfda65f9c3c8888f352b825
Iván Bautista <ivanbautistag@gmail.com>
2024-10-20 13:40:58 -0600

    feat: shadcn components

--- src/components/ui/alert.tsx
+++ src/components/ui/alert.tsx
@@ -5,13 +5,15 @@ import { cva, type VariantProps } from 'class-variance-authority'
 import { cn } from '@/lib/utils'
 
 const alertVariants = cva(
-  'relative w-full rounded-lg border px-4 py-3 text-sm [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-4 [&>svg]:top-4 [&>svg]:text-foreground [&>svg~*]:pl-7',
+  'relative w-full rounded-lg border p-4 [&>svg~*]:pl-7 [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-4 [&>svg]:top-4 [&>svg]:text-foreground',
   {
     variants: {
       variant: {
         default: 'bg-background text-foreground',
         destructive:
-          'border-destructive/50 text-destructive dark:border-destructive [&>svg]:text-destructive',
+          'border-destructive text-destructive dark:border-destructive [&>svg]:text-destructive',
+        success:
+          'border-green-600 text-green-700/80 dark:border-green-400 [&>svg]:text-green-700/80',
       },
     },
     defaultVariants: {
