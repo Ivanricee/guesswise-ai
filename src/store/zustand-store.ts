d81c311571d92f317f2ac0cdd4fc8033e964a27a
Iván Bautista <ivanbautistag@gmail.com>
2024-10-20 13:43:25 -0600

    feat add wordtogess and score to player, gamestarted to start game, maxcounplayers to count rounds, openaikey to store the api key and isInlimit to validate only 5 players

--- src/store/zustand-store.ts
+++ src/store/zustand-store.ts
@@ -8,20 +8,45 @@ export type Player = {
   isHost: boolean
   isGuessing: boolean
   isCurrent: boolean
+  wordToGuess: string
+  score: number
 }
 interface State {
   players: Player[] | null
   token: string | null
+  gameStarted: boolean
+  maxCountPlayers: number
+  openAiKey: string | null
+  isInlimit: boolean | null
 }
+
 interface Actions {
   setPlayer: ({ player, token }: { player: Player; token: string | null }) => void
   removePlayer: ({ imageUrl, name }: { imageUrl: string; name: string }) => void
-  updatePlayer: ({ imageUrl, name }: { imageUrl: string; name: string }) => void
+  updatePlayer: ({
+    imageUrl,
+    name,
+    isGuessing,
+  }: {
+    imageUrl: string
+    name: string
+    isGuessing: boolean
+  }) => void
+  updatePlayers: () => void
+  setGameStarted: (gameStarted: boolean, maxCountPlayers: number) => void
+  setOpenAiKey: (openAiKey: string | null) => void
+  setMaxCountPlayers: (maxCountPlayers: number) => void
+  setIsInlimit: (isInlimit: boolean | null) => void
 }
 
 export const useAppStore = create<State & Actions>((set) => ({
   players: null,
   token: null,
+  gameStarted: false,
+  maxCountPlayers: 0,
+  openAiKey: null,
+  isInlimit: null,
+  setIsInlimit: (isInlimit: boolean | null) => set(() => ({ isInlimit: isInlimit })),
   setPlayer: ({ player, token }: { player: Player; token: string | null }) =>
     set((prevState) => {
       if (prevState.players) {
@@ -43,12 +68,20 @@ export const useAppStore = create<State & Actions>((set) => ({
       }
       return { players: [] }
     }),
-  updatePlayer: ({ imageUrl, name }: { imageUrl: string; name: string }) =>
+  updatePlayer: ({
+    imageUrl,
+    name,
+    isGuessing,
+  }: {
+    imageUrl: string
+    name: string
+    isGuessing: boolean
+  }) =>
     set((prevState) => {
       if (prevState.players) {
         const updatedPlayers = prevState.players.map((player) => {
           if (`${player.imageUrl}${player.name}` === `${imageUrl}${name}`) {
-            return { ...player, isGuessing: false }
+            return { ...player, isGuessing }
           }
           return player
         })
@@ -56,4 +89,22 @@ export const useAppStore = create<State & Actions>((set) => ({
       }
       return { players: [] }
     }),
+  updatePlayers: () =>
+    set((prevState) => {
+      const updatedPlayers = prevState.players?.map((player) => ({ ...player, isGuessing: true }))
+      return { players: updatedPlayers }
+    }),
+  setGameStarted: (gameStarted: boolean, maxCountPlayers: number) =>
+    set(() => ({
+      gameStarted,
+      maxCountPlayers,
+    })),
+  setOpenAiKey: (openAiKey: string | null) =>
+    set(() => ({
+      openAiKey,
+    })),
+  setMaxCountPlayers: (maxCountPlayers: number) =>
+    set(() => ({
+      maxCountPlayers,
+    })),
 }))
