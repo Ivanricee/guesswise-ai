303dcd623204c1a3e1add0d59ef70140171cca60
Iván Bautista <ivanbautistag@gmail.com>
2024-10-22 23:01:10 -0600

    feat: fix image to 1024 x 1024

--- src/lib/testImage.ts
+++ src/lib/testImage.ts
@@ -1,9 +1,10 @@
+/* eslint-disable max-len */
 import { CloudinaryImage } from './cloudinary'
 
 export const testImages: CloudinaryImage[] = [
   {
     asset_id: 'e35dac5a4dfd8a34329fde3fd055db79',
-    url: 'http://res.cloudinary.com/ivanrice-c/image/upload/v1728798386/Frontend/guesswise/players/local/atest_ai_image_ae74ck.png',
+    url: 'https://res.cloudinary.com/ivanrice-c/image/upload/c_pad,h_1024,w_1024/v1728798386/Frontend/guesswise/players/local/atest_ai_image_ae74ck.png',
     height: 521,
     width: 782,
     tags: [
