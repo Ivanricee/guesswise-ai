e3fd8c423aa5e95fd58df86de4264f61edac6d60
Iván Bautista <ivanbautistag@gmail.com>
2024-10-22 22:51:11 -0600

    feat: allow canva in next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      canvas: false, // Agregar esto para evitar problemas de importación de canvas
+    }
+    return config
+  },
   images: {
     remotePatterns: [
       {
